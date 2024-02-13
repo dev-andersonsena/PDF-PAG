4de849908f036ed7925fc6a323ed3b4301f92dd9
Dev-Andersonsena <dev.fullstack.anderson@gmail.com>
2024-02-13 00:54:09 -0300

    primeiro commit pdf

--- gerador/urls.py
+++ gerador/urls.py
@@ -3,6 +3,6 @@ from .views import upload_pdf, count_pages_view, pdf_page
 
 urlpatterns = [
     path('upload_pdf', upload_pdf, name='upload_pdf'),
-    path('count_pages/', count_pages_view, name='count_pages_view'),
+    path('count_pages/', count_pages_view, name='cou nt_pages_view'),
     path('', pdf_page, name='pdf_page'),
 ]
